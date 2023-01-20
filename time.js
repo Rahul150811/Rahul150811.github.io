e39a5eb09091af53855d6c4919dda5489d76c9ae
Rahul150811 <fnu.rahul25@student.qcc.cuny.edu>
2023-01-19 22:07:00 -0500

    Initial Commit

---
+++ time.js
@@ -0,0 +1,8 @@
+const dateDiv = document.getElementById('date-div');
+
+function myDateFunction() {
+  const now = new Date();
+  const nowStr = now.toLocaleString('en-US');
+  dateDiv.innerHTML = nowStr;
+}
+setInterval(myDateFunction, 1000);
