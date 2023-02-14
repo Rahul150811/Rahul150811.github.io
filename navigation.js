31ef413adad41a4e2e93431cbb18eb3790129415
Rahul150811 <fnu.rahul25@student.qcc.cuny.edu>
2023-02-13 19:48:17 -0500

    Initial Commit

--- navigation.js
+++ navigation.js
@@ -1,15 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-          document.getElementById('nav_wrapper').classList.add('fixed-top');
-          document.getElementById('nav_wrapper').classList.add('nav-colored');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('#nav').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('nav_wrapper').classList.remove('fixed-top');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
+function myFunction() {
+  var x = document.getElementById("centered_nav");
+  if (x.className === "rc_nav") {
+      x.className += " responsive";
+  } else {
+      x.className = "rc_nav";
+  }
+}
